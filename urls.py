5914e45279493a521d5ae7ce2bdd14e422784a39
nurana18 <nuranealiyarli120@gmail.com>
2025-05-21 19:50:56 +0400

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,7 +1,9 @@
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from .views import NewPost, text,Task
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('post/', include('post.urls'))
+    path('post/', text),
+    path('task/', Task.as_view(), name='task'),
+    path('test/', NewPost.as_view(), name='test')
+    
 ]
